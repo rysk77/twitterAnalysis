4f82302e51ccdacc1734e426adbb67323c505231
rysk77 <61385830+rysk77@users.noreply.github.com>
2020-06-03 08:25:22 +0900

    Add files via upload

--- analysis.php
+++ analysis.php
@@ -22,43 +22,38 @@
   <head>
     <meta charset="utf-8">
 	  <title>login</title>
+    <link rel="stylesheet" href="stylesheet.css">
 	</head>
 	<body>
-    <h1>キーワード検索画面</h1>
-    <p>プロフィール文にキーワードが含まれるフォロワーを表示します。</p>
-    <form class="" action="analysis.php" method="get" id="form">
-      <input type="text" name="keyword"　placeholder="検索キーワード" >
-      <input type="submit" id="submit"　name="submit" value="検索">
-    </form>
-    <?php if (isset($_GET['keyword'])) : ?>
-      <p><?= $keyword ?>の検索結果 <?= $count ?>/<?= $_SESSION['followers_count']?></p>
-      <table>
-        <tr>
-          <th>アイコン</th>
-          <th>名前</th>
-          <th>フォロー</th>
-          <th>フォロワー</th>
-        </tr>
-       <?php foreach ($results as $result) : ?>
+    <header>
+        <h1>フォロワーキーワード検索アプリ</h1>
+    </header>
+    <div class="container">
+      <p>キーワード検索</p>
+      <form  action="analysis.php" method="get" id="form">
+        <input type="text" name="keyword" >
+        <input class="submit" type="submit" name="submit" value="検索">
+      </form>
+      <?php if (isset($_GET['keyword'])) : ?>
+        <p><?= $keyword ?>の検索結果 <?= $count ?>/<?= $_SESSION['followers_count']?></p>
+        <table>
           <tr>
-            <th><img src="<?= $result['icon'] ?>"></th>
-            <th><?= $result['name'] ?></th>
-            <th><?= $result['friend'] ?></th>
-            <th><?= $result['follower'] ?></th>
+            <th></th>
+            <th class="name">名前</th>
+            <th>フォロー</th>
+            <th>フォロワー</th>
           </tr>
-        <?php endforeach; ?>
-      </table>
-    <?php endif; ?>
-  <script type="text/javascript">
-    //入力チェック
-    $("#form").submit(function(){
-      if ($("input[name='keyword']").val() == '') {
-      alert('キーワードを入力してください');
-      return false;
-      } else {
-        $("#form").submit();
-      }
-    });
-  </script>
+         <?php foreach ($results as $result) : ?>
+            <tr>
+              <td><img src="<?= $result['icon'] ?>"></td>
+              <td class="name"><?= $result['name'] ?></td>
+              <td><?= $result['friend'] ?></td>
+              <td><?= $result['follower'] ?></td>
+            </tr>
+          <?php endforeach; ?>
+        </table>
+      <?php endif; ?>
+    </div>
+  <footer><p>Copyright (C) 2020 Ryo. all rights reserved.</p></footer>
   </body>
 </html>
